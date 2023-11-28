619fc220bef3a63b34bb8e4766361718446de216
cksruthwik <66819110+cksruthwik@users.noreply.github.com>
2023-11-28 16:43:59 +0530

    Update CalculatorApplication.java

--- src/main/java/CalculatorApplication.java
+++ src/main/java/CalculatorApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorApplication {
 
     public static void main(String[] args) {
-        //root
+        //root 1.0
         SpringApplication.run(CalculatorApplication.class, args);
     }
 }
